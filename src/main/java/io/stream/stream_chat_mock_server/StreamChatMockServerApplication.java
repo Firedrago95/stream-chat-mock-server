9c788649740382c3a6e651fbf1a0a7b49f5db994
Firedrago95 <yonghwa95@naver.com>
2026-04-10 14:12:33 +0900

    chore: Spring Boot 기반 웹소켓 목 서버 초기 설정 추가
    - 프로젝트 초기화 및 기본 디렉토리 구조 추가
    - Spring Boot 의존성과 build.gradle 설정 파일 작성
    - Gradle Wrapper 및 필수 프로퍼티 파일 추가
    - README와 .gitignore, .gitattributes 작성
    - 간단한 테스트 클래스와 애플리케이션 엔트리포인트 구현

---
+++ src/main/java/io/stream/stream_chat_mock_server/StreamChatMockServerApplication.java
@@ -0,0 +1,13 @@
+package io.stream.stream_chat_mock_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamChatMockServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamChatMockServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/stream/stream_chat_mock_server/StreamChatMockServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stream.stream_chat_mock_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamChatMockServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
